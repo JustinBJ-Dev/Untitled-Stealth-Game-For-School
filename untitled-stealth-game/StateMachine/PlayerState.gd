4af31f56d7868283dc969177525842f156644cdd
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2026-08-31 14:12:37 -0400

    Started player development

---
+++ untitled-stealth-game/StateMachine/PlayerState.gd
@@ -0,0 +1,15 @@
+class_name PlayerState extends State
+
+@export var player_ : player
+
+func enter_state() -> void:
+	pass
+
+func exit_state() -> void:
+	pass
+
+func update(delta: float) -> void:
+	pass
+
+func physics_update(delta: float) -> void:
+	pass
