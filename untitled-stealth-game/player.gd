d4b9b71ce24b1ba00cb0d3be7e2dea2405e0e31d
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2026-08-31 19:03:49 -0400

    Updated game settings

--- untitled-stealth-game/player.gd
+++ untitled-stealth-game/player.gd
@@ -1,9 +1,14 @@
 class_name  player extends CharacterBody2D
 
+var dirX: float
+var dirY: float
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var playerInput  : Array
 
+func _process(delta):
+	dirX = Input.get_axis("input_left", "input_right")
+	dirY = Input.get_axis("input_up", "input_down")
+	
 
 func _physics_process(delta: float) -> void:
 
--- untitled-stealth-game/player_run.gd
+++ untitled-stealth-game/player_run.gd
@@ -1,1 +1,14 @@
 extends PlayerState
+
+@export var Idle_State : PlayerState
+
+@export_category("Properties")
+@export var SPEED : int = 300
+
+func update(delta: float) -> void:
+	if player_.dirX == 0 && player_.dirY == 0:
+		switch_state.emit(Idle_State)
+
+func physics_update(delta: float) -> void:
+	player_.velocity.x = SPEED * player_.dirX
+	player_.velocity.y = SPEED * player_.dirY
