d69bc832146bdc9d96157fe57be26f25c5091d79
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2026-09-05 09:30:06 -0400

    Starting player dash

--- untitled-stealth-game/Player/player.gd
+++ untitled-stealth-game/Player/player.gd
@@ -3,10 +3,13 @@ class_name  player extends CharacterBody2D
 var dirX: float
 var dirY: float
 var playerInput  : Vector2
+var playerDirection : Vector2
 
 var playerHealth : float = 3.0
+var playerEnergy : float = 100
 
 @export var healthBar : ProgressBar
+@export var energyBar : ProgressBar
 
 func _process(_delta):
 	dirX = Input.get_axis("input_left", "input_right")
@@ -14,14 +17,16 @@ func _process(_delta):
 	playerInput.x = dirX
 	playerInput.y = dirY
 	playerInput = playerInput.normalized()
+	playerDirection = Vector2(cos(rotation),sin(rotation)).normalized()
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		playerHealth -= 1
+		playerEnergy -= 100
 	
 	manage_ui()
 
 func manage_ui() -> void:
 	healthBar.value = playerHealth
+	energyBar.value = playerEnergy
 	pass
 
 func _physics_process(_delta: float) -> void:
